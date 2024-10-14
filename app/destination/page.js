bc89ec74eed4b9df69d8c43a6ed1f92b64517f43
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-10-14 22:33:13 +0200

    added the functionality to reflect the add or remove of planets and display the number of selected planets accordingly

--- app/destination/page.js
+++ app/destination/page.js
@@ -44,38 +44,43 @@ export const Destinations = () => {
       description:
         "Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers.",
       thumbnail: "/destination/image-europa.png",
+      isSelected: false,
     },
     {
       name: "Mars",
       description:
         "Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth.",
       thumbnail: "/destination/image-mars.png",
+      isSelected: false,
     },
     {
       name: "Moon",
       description:
         "Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers.",
       thumbnail: "/destination/image-moon.png",
+      isSelected: false,
     },
     {
       name: "Titan",
       description:
         "Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets.",
       thumbnail: "/destination/image-titan.png",
+      isSelected: false,
     },
   ];
   const [selectedPlanets, onAddPlanet] = useState([]);
-  const [planetSelected, setPlanetSelected] =useState(false);
-  // let isPlanetSelected = false;
-  let numberOfPlanets = 0;
+  const [planetArray, setPlanetArray] = useState(planetList);
 
   const onAddOrRemovePlanet = (name, index) => {
-    // TASK - React 1 week 2
-    // Implement this function
-    // If you press the "ADD PLANET" the selected planet should display "SELECTED"
-    // And the counter should update, how many planets are selected (numberOfPlanets)
+    const newPlanetList = [...planetArray];
+    newPlanetList[index].isSelected = !newPlanetList[index].isSelected;
+    setPlanetArray(newPlanetList);
+    const selectedPlanetList = planetArray.filter(
+      (planet) => planet.isSelected === true
+    );
+    onAddPlanet(selectedPlanetList);
     console.log(
-      `You seleceted the following planet: ${name}, with the index of ${index}`
+      `You selected the following planet: ${name}, with the index of ${index}`
     );
   };
 
@@ -85,11 +90,11 @@ export const Destinations = () => {
         <h1>Travel destinations</h1>
         <section className="card">
           <h2>Wishlist</h2>
-          {/* TASK - React 1 week 2 */}
-          {/* Display the number Of selected planets */}
-          {/* Display the "no planets" message if it is empty! */}
-          <p>No planets in wishlist :(</p>
-          <p>You have {numberOfPlanets} in your wishlist</p>
+          <p>
+            You have{" "}
+            {selectedPlanets.length > 0 ? selectedPlanets.length : "no planets"}{" "}
+            in your wishlist
+          </p>
           <b>List coming soon after lesson 3!</b>
 
           {/* STOP! - this is for week 3!*/}
@@ -117,18 +122,13 @@ export const Destinations = () => {
         </section>
         <section className="card">
           <h2>Possible destinations</h2>
-          {/* TASK - React 1 week 2 */}
-          {/* Add all 4 planets! Europa, Moon, Mars, Titan  */}
-          {/* Use the README.md file for descriptions */}
-          {/* Create a <PlanetCard /> component, which accepts the following properties: */}
-          {/* name, description, thumbnail, isSelected, onAddOrRemovePlanet */}
-          {planetList.map((planet,index) => (
+          {planetArray.map((planet, index) => (
             <PlanetCard
               key={planet.name}
               name={planet.name}
               description={planet.description}
               thumbnail={planet.thumbnail}
-              isSelected={planetSelected}
+              isSelected={planet.isSelected}
               index={index}
               onAddOrRemovePlanet={onAddOrRemovePlanet}
             />
