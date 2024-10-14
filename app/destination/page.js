1679031a79728706739e3c786aa9abc7b417f670
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-10-14 16:11:28 +0200

    created a component PlanetCard along with the props

--- app/destination/page.js
+++ app/destination/page.js
@@ -5,10 +5,68 @@ import { useState } from "react";
 import styles from "@/components/destination/destination.module.css";
 import { AddWishlistItem } from "@/components/destination/AddWishlistItem";
 
-export const Destinations = () => {
-  const [selectedPlanets, onAddPlanet] = useState([]);
+const PlanetCard = ({
+  name,
+  description,
+  thumbnail,
+  isSelected,
+  index,
+  onAddOrRemovePlanet,
+}) => {
+  return (
+    <div className={styles.planetCard}>
+      <img
+        className={styles.planetThumbnail}
+        src={thumbnail}
+        alt={`image of ${name}`}
+      />
+      <div className={styles.planetDescription}>
+        <h2>
+          {name}
+          {isSelected ? "- SELECTED" : ""}
+        </h2>
+        <p>{description}</p>
+      </div>
+      <button
+        className="roundButton"
+        onClick={() => onAddOrRemovePlanet(name, index)}
+      >
+        {isSelected ? "REMOVE" : "ADD PLANET"}
+      </button>
+    </div>
+  );
+};
 
-  let isPlanetSelected = false;
+export const Destinations = () => {
+  const planetList = [
+    {
+      name: "Europa",
+      description:
+        "Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers.",
+      thumbnail: "/destination/image-europa.png",
+    },
+    {
+      name: "Mars",
+      description:
+        "Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth.",
+      thumbnail: "/destination/image-mars.png",
+    },
+    {
+      name: "Moon",
+      description:
+        "Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers.",
+      thumbnail: "/destination/image-moon.png",
+    },
+    {
+      name: "Titan",
+      description:
+        "Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets.",
+      thumbnail: "/destination/image-titan.png",
+    },
+  ];
+  const [selectedPlanets, onAddPlanet] = useState([]);
+  const [planetSelected, setPlanetSelected] =useState(false);
+  // let isPlanetSelected = false;
   let numberOfPlanets = 0;
 
   const onAddOrRemovePlanet = (name, index) => {
@@ -64,40 +122,17 @@ export const Destinations = () => {
           {/* Use the README.md file for descriptions */}
           {/* Create a <PlanetCard /> component, which accepts the following properties: */}
           {/* name, description, thumbnail, isSelected, onAddOrRemovePlanet */}
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-europa.png"
-              alt=""
+          {planetList.map((planet,index) => (
+            <PlanetCard
+              key={planet.name}
+              name={planet.name}
+              description={planet.description}
+              thumbnail={planet.thumbnail}
+              isSelected={planetSelected}
+              index={index}
+              onAddOrRemovePlanet={onAddOrRemovePlanet}
             />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => onAddOrRemovePlanet("Pluto", 0)}
-            >
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-europa.png"
-              alt=""
-            />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => onAddOrRemovePlanet("Pluto", 0)}
-            >
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
+          ))}
         </section>
       </main>
     </div>
